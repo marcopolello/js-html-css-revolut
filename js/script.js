f6fd2d91bcfd7145fa98f7fa1b2758837ce2daee
marcopolello <marco-pol@hotmail.it>
2020-11-04 19:17:31 +0100

    ex finito

--- js/script.js
+++ js/script.js
@@ -1,26 +1,50 @@
+/*
 $(document).ready(function(){
+
 
   $('li.dropitem').each(function(indice, elemento){
     $(this).click(function() {
-      console.log("hai cliccato correttamente");
-      console.log(indice, elemento); //elemento = $(this)
-      $('nav.dropdown').removeClass('dispblock');
+      // console.log("hai cliccato correttamente");
+      // console.log(indice, elemento); //elemento = $(this)
       if (indice === 0) {
-        $('#dd1').toggleClass('dispblock');
+        $('#dd1').toggle();
       } else if (indice === 1) {
-        $('#dd2').toggleClass('dispblock').css("left", "-20px");
+        $('#dd2').toggle().css("left", "-20px");
       } else if (indice === 2) {
-        $('#dd3').toggleClass('dispblock').css("left", "-15px");
+        $('#dd3').toggle().css("left", "-15px");
       } else if (indice === 3) {
-        $('#dd4').toggleClass('dispblock').css("left", "-13px");
+        $('#dd4').toggle().css("left", "-13px");
       } else if (indice === 4) {
-        $('#dd5').toggleClass('dispblock').css("left", "-18px");
+        $('#dd5').toggle().css("left", "-18px");
       } else if (indice === 5) {
-        $('#dd6').toggleClass('dispblock').css("left", "-13px");
+        $('#dd6').toggle().css("left", "-13px");
       }
-
     });
 
   });
 
-});
+}); */
+
+
+// 2 METODO (risolto bug per cui non si chiudeva menu all'apertura di un altro)
+
+
+$(document).ready(function () {
+
+ var dropItem =  $(".dropitem");
+ //funzione che parte al click di un <li> con classe .dropitem
+ dropItem.click(function () {
+   // ogni volta che clicco un li tutti gli li di classe .dropdown salgono
+   $(".dropdown").slideUp();
+   // al click dello stesso va su e giù
+   $(this).children(".dropdown").toggle();
+ });
+
+ // cambio proprietà css dei menu dropdown tramite selezione id
+ $('#dd2').css("left", "-20px");
+ $('#dd3').css("left", "-15px");
+ $('#dd4').css("left", "-13px");
+ $('#dd5').css("left", "-18px");
+ $('#dd6').css("left", "-13px");
+
+}); 
