f52574202decfe20953eccd292347c2085a281ce
marcopolello <marco-pol@hotmail.it>
2020-11-04 17:24:05 +0100

    ogni link ha il suo nav interno

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $('li.dropitem').each(function(indice, elemento){
+    $(this).click(function() {
+      console.log("hai cliccato correttamente");
+      console.log(indice, elemento); //elemento = $(this)
+      if (indice === 0) {
+        $('#dd1').toggleClass('dispblock');
+      } else if (indice === 1) {
+        $('#dd2').toggleClass('dispblock').css("left", "-20px");
+      } else if (indice === 2) {
+        $('#dd3').toggleClass('dispblock').css("left", "-15px");
+      } else if (indice === 3) {
+        $('#dd4').toggleClass('dispblock').css("left", "-13px");
+      } else if (indice === 4) {
+        $('#dd5').toggleClass('dispblock').css("left", "-18px");
+      } else if (indice === 5) {
+        $('#dd6').toggleClass('dispblock').css("left", "-13px");
+      }
+
+    });
+
+  });
+
+
+/*
+  $('li.dropitem').click(function() {
+    // console.log($(this));
+    $(this).
+  });
+
+  function changeDisplay() {
+    $('.dropdown').toggleClass('dispblock');
+  }
+*/
+
+
+});
