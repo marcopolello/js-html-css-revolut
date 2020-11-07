95caf23374f5443ec9ef26305b0785501602c827
marcopolello <marco-pol@hotmail.it>
2020-11-07 16:21:35 +0100

    modifiche css all'header, ancora da finire parte responsive

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-/*
-$(document).ready(function(){
-
-
-  $('li.dropitem').each(function(indice, elemento){
-    $(this).click(function() {
-      // console.log("hai cliccato correttamente");
-      // console.log(indice, elemento); //elemento = $(this)
-      if (indice === 0) {
-        $('#dd1').toggle();
-      } else if (indice === 1) {
-        $('#dd2').toggle().css("left", "-20px");
-      } else if (indice === 2) {
-        $('#dd3').toggle().css("left", "-15px");
-      } else if (indice === 3) {
-        $('#dd4').toggle().css("left", "-13px");
-      } else if (indice === 4) {
-        $('#dd5').toggle().css("left", "-18px");
-      } else if (indice === 5) {
-        $('#dd6').toggle().css("left", "-13px");
-      }
-    });
-
-  });
-
-}); */
-
-
-// 2 METODO (risolto bug per cui non si chiudeva menu all'apertura di un altro)
-
-
 $(document).ready(function () {
 
  var dropItem =  $(".dropitem");
